873f5e49a659f03515ac4ba917904af5e2a0b086
Sophie <seidels@ethz.ch>
2022-09-15 17:06:39 +0200

    Adapt for new clock model settings

--- Fig3/3_analysis/computeTopologyScores.R
+++ Fig3/3_analysis/computeTopologyScores.R
@@ -1,12 +1,51 @@
+## ---------------------------
+##
+## Script name: computeTologoyScores.R
+##
+## Purpose of script: Calculate the score assessing tree topology
+##
+## Author: Sophie Seidel
+##
+## Date Created: 2022-08-17
+##
+## Copyright (c) Sophie Seidel, 2022
+## Email: sophie.seidel@posteo.de
+##
+## ---------------------------
+##
+## Notes: Adjust the subDirIdx from 1-3 to compute the
+##  topology scores for all 3 analyses
+##
+## ---------------------------
+
+## set working directory for Mac
+
+setwd("~/Projects/trees-in-devBio/papers/bayesianPhylogeneticInference/Figures/Fig3/3_analysis/")
+
+## ---------------------------
+
+options(scipen = 6, digits = 4) # I prefer to view outputs in non-scientific notation
+
+## ---------------------------
+
+## load up our functions into memory
+
 source("tree_scripts.R")
 
-# Convert the mcc trees from nexus into newick files, s.t. TreeCmp can process them
+## ---------------------------
+
+## init directories and find mcc (maximum clade credibility) tree files
+
 intmemdir = "../2_inference/inference_logs/"
-subdirs = c("priorInfoOnScarring/", "unsupervised/")
-subdirIdx = 2
+subdirs = c("1clock_2scars/", "1clock_20scars/", "10clocks_20scars/")
+subdirIdx = 3 # run consecutively for 1:3
 
 intmemdir = paste0(intmemdir, subdirs[subdirIdx])
 files = list.files(intmemdir, pattern = "mcc.tree")
+
+## ---------------------------
+
+## Convert the mcc trees from nexus into newick files, s.t. TreeCmp can process them
 
 for (nexusFile in files){
   print(nexusFile)
@@ -19,7 +58,10 @@ for (nexusFile in files){
 
 }
 
-# run TreeCmp
+## ---------------------------
+
+## run TreeCmp to score the estimated tree topology
+
 newickFiles = list.files(path = intmemdir, pattern = "*.newick")
 trueTreeDir = "../dat/trueTrees/"
 scoreDir = paste0("../2_inference/treecmpScores/",
