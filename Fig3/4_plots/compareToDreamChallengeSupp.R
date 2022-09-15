9f53cc46a6cad08252e197487bee48ab08ae5595
Sophie <seidels@ethz.ch>
2022-09-15 17:07:17 +0200

    Add plot scripts for new clock model settings

--- Fig3/4_plots/compareToDreamChallengeSupp.R
+++ Fig3/4_plots/compareToDreamChallengeSupp.R
@@ -43,7 +43,7 @@ height = 8
 unit = "cm"
 font = "Arial"
 text_size = 12
-mycolors_ =carto_pal(n = nColor, "Safe")[c(1,4,3,2,5:8)]
+mycolors_ =carto_pal(n = nColor, "Safe")[c(1,4,4,4,3,2,5,6)]
 
 plotDir = "../4_plots/"
 
@@ -52,9 +52,10 @@ plotDir = "../4_plots/"
 ## collect scores
 scoresF = "../dat/dreamChallengeScoresRed.csv"
 scores = read.csv(scoresF, stringsAsFactors = F)
-scores[10, ] = c(9, "13.1.22", "TiDeTree", 0.55, 0.55, "?") # from scores.tsv in 3_analysis
+scores[10, ] = c(9, "13.1.22", "TiDeTree-c", 0.55, 0.55, "?") # from scores.tsv in 3_analysis
 scores[11, ] = c(10, "12.1.21", "AMbeRland*", 0.52, 0.52, "?")
-
+scores[12, ] = c(11, "13.1.22", "TiDeTree-b", 0.55, 0.55 , "?") # from scores.tsv in 3_analysis
+scores[13, ] = c(12, "13.1.22", "TiDeTree-a", 0.56, 0.57 , "?") # from scores.tsv in 3_analysis
 scores[1:5,3] = c("Cassiopeia", "Guan Lab", "Jasper06", "pRennert", "RnILabs")
 scores[7, 3] = "WhatATeam"
 
@@ -70,16 +71,17 @@ scores = scores[1:8, ]
 
 ## create different shape for TiDeTree
 scores$submitterId = factor(scores$submitterId, levels = scores$submitterId)
-scores$shape = scores$submitterId == "TiDeTree"
+scores$shape = (startsWith(x = as.character(scores$submitterId), "TiDeTree"))
+scores$shape[2:4] = c(1,2,3)
 
 ## create plot
 
 g = ggplot(scores, aes(y=Triples_average, x=RF_average, col=submitterId, fill=submitterId))+
-    geom_point(aes(shape=shape), size=5)+
+    geom_point(aes(shape=as.factor(shape)), size=5)+
     theme_classic()+
     scale_color_manual(values = mycolors_)+
     scale_fill_manual(values = mycolors_)+
-    scale_shape_manual(values = c(3,22))+
+    scale_shape_manual(values = c(3,22,23,24))+
     xlim(0.49,0.9) + ylim(0.49,0.9)+
     theme(legend.position = c(0.8, 0.7))+
     ylab("Mean RF distance") +
@@ -90,7 +92,7 @@ g = ggplot(scores, aes(y=Triples_average, x=RF_average, col=submitterId, fill=su
     guides(shape="none")
 g
 
-svg(filename = paste0(plotDir, "topologyRanking.svg"),
+svg(filename = paste0(plotDir, "topologyRanking_supp.svg"),
     width = width, height = height, family = font,)
 g
 dev.off()
