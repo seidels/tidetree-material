39040f712b82cdb04d80c8cb9cc79a56410c559c
Sophie <seidels@ethz.ch>
2022-09-15 17:06:18 +0200

    Add header

--- Fig3/3_analysis/combineTopologyScores.R
+++ Fig3/3_analysis/combineTopologyScores.R
@@ -1,9 +1,42 @@
-scoreDir ="../dat/treecmpScores/"
-subdirs = c("priorInfoOnScarring/", "unsupervised/" )
-subdirIdx = 2
+## ---------------------------
+##
+## Script name: combineTopologyScores.R
+##
+## Purpose of script: Compute the average topology scores
+##  across all trees for each analysis and save in scores.tsv
+##
+## Author: Sophie Seidel
+##
+## Date Created: 2022-08-17
+##
+## Copyright (c) Sophie Seidel, 2022
+## Email: sophie.seidel@posteo.de
+##
+## ---------------------------
+##
+## Notes: Run with subDirIdx from 1-3 to run each analysis
+##
+##
+## ---------------------------
+
+## set working directory for Mac
+
+setwd("~/Projects/trees-in-devBio/papers/bayesianPhylogeneticInference/Figures/Fig3/3_analysis/")      # Sophie's working directory (mac)
+
+## ---------------------------
+
+## set up dirs
+scoreDir ="../2_inference/treecmpScores/"
+subdirs = c("1clock_2scars/", "1clock_20scars/", "10clocks_20scars/")
+subdirIdx = 3
 scoreDir = paste0(scoreDir, subdirs[subdirIdx])
 
 scoreFiles = list.files(scoreDir, pattern = "dreamChallenge")
+
+
+## ---------------------------
+
+## collect scores from files and store in data frame
 
 scores = data.frame(file=scoreFiles, RF=0, triplet=0)
 
@@ -18,7 +51,9 @@ for (i in 1:length(scoreFiles)){
   scores[i, "triplet"] = score$Triples_toYuleAvg[1]
 }
 
-#mean metrics across all 93 colonies
+## ---------------------------
+
+#mean metrics across all 106 colonies
 round(mean(as.numeric(scores$RF) , na.rm = T), digits = 2)
 round(mean(as.numeric(scores$triplet), na.rm = T), digits = 2)
 
@@ -32,6 +67,8 @@ scores$fileName = sapply(X = scores$file, function(x){strsplit(x, split = "treec
 
 merged = merge(scores, challenge, by = "fileName")
 
-round(mean(as.numeric(merged[which(merged$partition == "test"), "RF.x"]), na.rm = T), digits = 2)
-round(mean(as.numeric(merged[which(merged$partition == "test"), "triplet"]), na.rm = T), digits = 2)
+mean_rf = round(mean(as.numeric(merged[which(merged$partition == "test"), "RF.x"]), na.rm = T), digits = 2)
+mean_triplet = round(mean(as.numeric(merged[which(merged$partition == "test"), "triplet"]), na.rm = T), digits = 2)
 
+write(x = c(subdirs[subdirIdx], mean_rf, mean_triplet), file = "scores.tsv",
+      append = T, sep = "\t", ncolumns = 3)
